ac0d69052dc050982cd510c9415f642023a00d4d
noctilucent <pedromarcano@outlook.cl>
2024-03-27 23:56:42 -0300

    Detalles esta listo sin comentarios

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -9,17 +9,12 @@ const Detail = () => {
 
   const [dentista, setDentista] = useState({});
   const { id } = useParams();
-  const [form, setForm] = useState({
-    name: "",
-    email:"",
-    phone: "",
-    website: "",
-  })
 
-useEffect(() => {
-    axios (`https://jsonplaceholder.typicode.com/users/` + id) //Usando axios porque fetch no funciono
-    .then (res => setDentista(res.data))
-  }, [id])
+  useEffect(() => {
+    axios(`https://jsonplaceholder.typicode.com/users/` + id).then((res) =>
+      setDentista(res.data)
+    );
+  }, [id]);
 
   if (!dentista) {
     return <div>Espere un momento...</div>;
@@ -31,15 +26,15 @@ useEffect(() => {
         style={{ color: theme.font, backgroundColor: theme.backgroundColor }}
       >
         <h1>Detalles </h1>
-
         <table>
-          <caption><h2>{dentista.name}</h2></caption>
+          <caption>
+            <h2>{dentista.name}</h2>
+          </caption>
           <tbody>
             <tr>
               <th>Correo</th>
               <th>Telefono</th>
               <th>Sitio web</th>
-              
             </tr>
             <tr>
               <td>{dentista.email}</td>
@@ -48,35 +43,6 @@ useEffect(() => {
             </tr>
           </tbody>
         </table>
-
-
-{/*         <table>
-          <tbody>
-            <tr>
-              <td>Nombre</td>
-              <td>{dentista.name}</td>
-            </tr>
-            <tr>
-              <td>Correo</td>
-              <td>dentista.email</td>
-            </tr>
-            <tr>
-              <td>Telefono</td>
-              <td>{dentista.phone}</td>
-            </tr>
-            <tr>
-              <td>Sitio web</td>
-              <td>{dentista.website}</td>
-            </tr>
-          </tbody>
-        </table> */}
-
-
-        {/*<p>Nombre: {dentista.name}</p>
-        <p>Correo: {dentista.email}</p>
-        <p>Telefono: {dentista.phone}</p>
-        <p>Sitio web: {dentista.website}</p> */}
-
       </div>
     </>
   );
