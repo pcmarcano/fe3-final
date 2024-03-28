c285b62d5a831075b400ed494402dfbbb718f3b7
noctilucent <pedromarcano@outlook.cl>
2024-03-27 23:54:59 -0300

    Detalle esta listo

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -9,9 +9,14 @@ const Detail = () => {
 
   const [dentista, setDentista] = useState({});
   const { id } = useParams();
+  const [form, setForm] = useState({
+    name: "",
+    email:"",
+    phone: "",
+    website: "",
+  })
 
-
-  useEffect(() => {
+useEffect(() => {
     axios (`https://jsonplaceholder.typicode.com/users/` + id) //Usando axios porque fetch no funciono
     .then (res => setDentista(res.data))
   }, [id])
@@ -26,10 +31,52 @@ const Detail = () => {
         style={{ color: theme.font, backgroundColor: theme.backgroundColor }}
       >
         <h1>Detalles </h1>
-        <p>Nombre: {dentista.name}</p>
+
+        <table>
+          <caption><h2>{dentista.name}</h2></caption>
+          <tbody>
+            <tr>
+              <th>Correo</th>
+              <th>Telefono</th>
+              <th>Sitio web</th>
+              
+            </tr>
+            <tr>
+              <td>{dentista.email}</td>
+              <td>{dentista.phone}</td>
+              <td>{dentista.website}</td>
+            </tr>
+          </tbody>
+        </table>
+
+
+{/*         <table>
+          <tbody>
+            <tr>
+              <td>Nombre</td>
+              <td>{dentista.name}</td>
+            </tr>
+            <tr>
+              <td>Correo</td>
+              <td>dentista.email</td>
+            </tr>
+            <tr>
+              <td>Telefono</td>
+              <td>{dentista.phone}</td>
+            </tr>
+            <tr>
+              <td>Sitio web</td>
+              <td>{dentista.website}</td>
+            </tr>
+          </tbody>
+        </table> */}
+
+
+        {/*<p>Nombre: {dentista.name}</p>
         <p>Correo: {dentista.email}</p>
         <p>Telefono: {dentista.phone}</p>
-        <p>Setio web: {dentista.website}</p>
+        <p>Sitio web: {dentista.website}</p> */}
+
       </div>
     </>
   );
